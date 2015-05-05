3ed7725fa0d8933a1b1845653d43c76c94cde006
Mike Sevilla <michaelsevilla@gmail.com>
2015-05-05 13:56:00 -0700

    tab views up

--- MyPantry/MyPantry/GoShoppingViewController.swift
+++ MyPantry/MyPantry/GoShoppingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class GoShoppingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ MyPantry/MyPantry/PantryListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PantryListViewController.swift
+//  MyPantry
+//
+//  Created by Mike Sevilla on 5/5/15.
+//  Copyright (c) 2015 Mike Sevilla. All rights reserved.
+//
+
+import UIKit
+
+class PantryListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
